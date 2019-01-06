0d2313f1a663fc34c67caee627f4789896d3fa7e
srigodavarti <slgodavarti@gmail.com>
2019-01-06 09:33:34 -0800

    Add files via upload

---
+++ tip/tip/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Srilakshmi Godavarti on 12/22/18.
+//  Copyright © 2018 Srilakshmi Godavarti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [ 0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
